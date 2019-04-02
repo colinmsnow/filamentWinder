53b94b81211b0fa7aed95a063c870d537792eeda
Colin Snow <colinmsnow@gmail.com>
2019-04-01 21:42:22 -0400

    Call Define Parameters

--- double_stepper.py
+++ double_stepper.py
@@ -351,6 +351,7 @@ class Stepper:
 			sleep(waitTime)
 
 testStepper = Winder([23, 24, 25, 22],[17, 27, 18, 10])
+testStepper.defineParameters(1000,3,.1)
 #testStepper.step(1,'left',stayOn = False )
 testStepper.home()
 testStepper.wrap90('right')
